30dab76336f39d8a9cc24477198d2172162a4eb5
François Poirotte <clicky@erebot.net>
2011-10-25 21:51:39 +0200

    Update dependencies
    - Remove PEAR packages for svn/git versioning.
    - Add pear/Console_CommandLine.

--- post-install.sh
+++ post-install.sh
@@ -5,10 +5,9 @@
 echo "Installing common packages/extensions..."
 for pkg in                          \
     pear/PHP_ParserGenerator-alpha  \
-    pear/VersionControl_SVN-alpha   \
-    pear/VersionControl_Git-alpha   \
     pear/File_Gettext-beta          \
     pear/PHP_CodeSniffer            \
+    pear/Console_CommandLine        \
 ; do
     echo "Installing '$pkg'"
     "$2/bin/pear" install -os $pkg
