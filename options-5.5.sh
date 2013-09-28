fc2d42f5a72b3f06153b0ba0bf9ee27c0a24fdce
François Poirotte <clicky@erebot.net>
2013-09-28 15:05:05 +0200

    Apply ICU patch before compiling.

--- options-5.5.sh
+++ options-5.5.sh
@@ -1,1 +1,20 @@
-options-5.4.sh
+#!/bin/sh
+
+pushd "php-$1"
+patch -N -p1 < ../custom/icu-pkg-config.diff
+PHP_AUTOCONF=autoconf ./buildconf --force
+popd
+
+configoptions="\
+$configoptions \
+--enable-simplexml=shared \
+--enable-hash \
+--enable-intl=shared \
+--enable-phar=shared \
+--with-sqlite3 \
+--with-mysql=shared,mysqlnd \
+--with-mysqli=shared,mysqlnd \
+--with-pdo-mysql=shared,mysqlnd \
+--enable-fpm \
+"
+
