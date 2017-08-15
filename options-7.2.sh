2c0c0a62ca6d5f44c1109157aaca3157249fadf5
François Poirotte <clicky@erebot.net>
2017-08-15 18:27:59 +0200

    Use autoconf 2.69 when reconfiguring PHP 7.2
    Previous versions used autoconf 2.59, but the version requirements have been
    upgraded for PHP 7.2.

--- options-7.2.sh
+++ options-7.2.sh
@@ -1,1 +1,26 @@
-options-7.1.sh
+#!/bin/bash
+
+pushd "php-$1"
+patch -r - -N -p1 < ../custom/icu-pkg-config.diff
+if [ $? -eq 0 ]; then
+    PHP_AUTOCONF=autoconf ./buildconf --force
+    touch --reference=buildconf configure
+fi
+popd
+
+configoptions="\
+$configoptions \
+--enable-simplexml=shared \
+--enable-hash \
+--enable-intl=shared \
+--enable-phar=shared \
+--with-mcrypt=shared \
+--with-sqlite3 \
+--with-mysqli=shared,mysqlnd \
+--with-pdo-mysql=shared,mysqlnd \
+--enable-opcache=shared \
+--enable-fpm \
+--enable-phpdbg \
+--enable-gcc-global-regs \
+"
+
