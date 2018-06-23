7044f65d58f91947dce9b395aaf7e2f98caef943
François Poirotte <clicky@erebot.net>
2018-06-23 23:22:24 +0200

    Add PHP 7.3.0 alpha 2 to compiled versions

---
+++ options-7.3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pushd "php-$1"
+patch -r - -N -p1 < ../custom/icu-pkg-config73.diff
+if [ $? -eq 0 ]; then
+    PHP_AUTOCONF=autoconf ./buildconf --force
+    touch --reference=buildconf configure
+fi
+popd
+
+configoptions="\
+$configoptions \
+--enable-simplexml=shared \
+--enable-hash \
+--enable-intl=shared \
+--enable-phar=shared \
+--with-sqlite3 \
+--with-mysqli=shared,mysqlnd \
+--with-pdo-mysql=shared,mysqlnd \
+--with-libzip \
+--with-password-argon2 \
+--with-sodium=shared \
+--enable-opcache=shared \
+--enable-fpm \
+--enable-phpdbg \
+--enable-gcc-global-regs \
+"
+
